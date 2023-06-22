c1fc1c9ce88843dab16ec89b45e81889c3f1c0b2
Fazt <fazt@faztweb.com>
2023-06-21 19:38:44 -0500

    better frontend routing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from routes.task import task
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="client/dist")
+
 
 origins = [
     "http://localhost:5173",
@@ -19,4 +22,7 @@ app.add_middleware(
 
 app.include_router(task)
 
-app.mount("/", StaticFiles(directory="client/dist", html=True), name="static")
+# static files for react app
+@app.get("/{rest_of_path:path}")
+async def react_app(req: Request, rest_of_path: str):
+    return templates.TemplateResponse('index.html', { 'request': req })
